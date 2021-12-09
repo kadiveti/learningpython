f353bc9662085ab3525f7b32c968a954daaa4330
kadiveti <saikumar0303@gmail.com>
2021-12-09 21:43:28 +0530

    Pyd1

---
+++ fuzzbuzz.py
@@ -0,0 +1,11 @@
+def fuzzbuzzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "fuzzbuzz"
+    if number % 3 == 0:
+        return "fuzz"
+    if number % 5 == 0:
+        return "buzz"
+    return number
+
+
+print(fuzzbuzzz(15))
