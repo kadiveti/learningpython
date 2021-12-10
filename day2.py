d72a546bbeedd3d6b4aee7c86f8977698c7f108c
kadiveti <saikumar0303@gmail.com>
2021-12-10 14:37:55 +0530

    Day21

---
+++ day2.py
@@ -0,0 +1,36 @@
+
+# # simple palendrone
+# # first_name = "Sai kumar kadiveti"
+# # print(first_name[::-1])
+
+# # # List unpacking
+# # numbers = [1, 2, 3, 4]
+# # first, second, *other = numbers
+# # print(first)
+# # print(other)
+
+# # looping over list
+
+# # for index, letter in enumerate(numbers):
+# #     print(index, letter)
+
+# # for letter in enumerate(numbers):
+# #     print(letter[0], letter[1])
+
+# letters = ["a", "b", "c", "b", "d", "b", "f", "b", "e"]
+
+# # adding in the list
+
+# letters.append("b")
+# letters.insert(1, "w")
+
+
+# # Remove from the list
+
+# # letters.pop()
+
+# for i in range(5):
+#     print(i)
+#     letters.remove("b")
+
+# print(letters)
